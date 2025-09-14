9bf9e8624871e765743ad8ae14234541469e3469
JamesJDill <j.8letters@gmail.com>
2025-09-14 12:03:06 -0400

    added linear assignment helper function

--- tracker/assignment.py
+++ tracker/assignment.py
@@ -1,4 +1,5 @@
 import numpy as np
+import lap
 
 def iou(box1, box2):
     x1_min, y1_min, x1_max, y1_max = box1
@@ -89,4 +90,17 @@ def cost_matrix(tracks: np.ndarray, detections: np.ndarray, alpha=0.7, gamma=1.5
     cdist = cdist_vectorized(tracks, detections)
     w = (1.0 - iou)**gamma
     
-    return (ones - iou) + (alpha * w * cdist)
+    return ((ones - iou) + (alpha * w * cdist)) / (1 + alpha)
+
+def linear_assignment(cost_matrix, thresh):
+    if cost_matrix.size == 0:
+        return np.empty((0, 2), dtype=int), tuple(range(cost_matrix.shape[0])), tuple(range(cost_matrix.shape[1]))
+    matches, unmatched_a, unmatched_b = [], [], []
+    cost, x, y = lap.lapjv(cost_matrix, extend_cost=True, cost_limit=thresh)
+    for ix, mx in enumerate(x):
+        if mx >= 0:
+            matches.append([ix, mx])
+    unmatched_a = np.where(x < 0)[0]
+    unmatched_b = np.where(y < 0)[0]
+    matches = np.asarray(matches)
+    return matches, unmatched_a, unmatched_b
